6d9cf1ea72426ec97b185a5435394736b7f4c8e1
ポキ <aman_a@aol.com>
2020-10-29 06:31:11 +0500

    added heroku support
    - for testing ofcourse

--- botapi/__init__.py
+++ botapi/__init__.py
@@ -4,6 +4,8 @@ from configparser import ConfigParser
 from pyrogram import Client
 
 PORT = environ.get('PORT', None)
+API_ID = environ.get('API_ID', None)
+API_HASH = environ.get('API_HASH', None)
 
 app = FastAPI()
 
@@ -13,8 +15,8 @@ config.read("botapi.ini")
 async def sessions(token):
     client = Client(
         ':memory:',
-        api_id=int(config.get('pyrogram', 'api_id')),
-        api_hash=config.get('pyrogram', 'api_hash'),
+        api_id=int(config.get('pyrogram', 'api_id') or API_ID),
+        api_hash=config.get('pyrogram', 'api_hash') or API_HASH,
         bot_token=token
     )
     await client.start()
